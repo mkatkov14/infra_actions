8782083d57de184fa5471b9e2c107912dc28b7a9
max katkov <lavka96@yandex.ru>
2022-07-24 08:28:50 +0500

    deploy test

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '51.250.126.13'
+    '51.250.126.13',
     'localhost',
     '127.0.0.1',
     '[::1]',
